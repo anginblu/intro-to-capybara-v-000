93e7fe87ea683bc875fcae8aa4e8f13ccb2b0de2
Jacqueline Lee <anginblu@gmail.com>
2018-01-03 18:55:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 class Application < Sinatra::Base
-  # Write your code here!
+  # Old route from previous tests
   get '/' do
-    erb :greet
+    erb :index
   end
+ 
+  # New route to respond to the form submission
   post '/greet' do
     erb :greet
   end
